cc85c15d7856be52963f31a31a917fbc5efeae2c
Anhquy150 <150499271+Anhquy150@users.noreply.github.com>
2023-11-16 21:06:45 +0700

    Add files via upload

---
+++ tra.php
@@ -0,0 +1,63 @@
+<div class="boxcf">
+    <p style="font-weight: bold; margin-top: 30px ; margin-left:20px">Trà</p>
+    <div class="cf">
+        <div class="col">
+            <a href=""><img src="img/đường đen sd.webp" alt="" width="250px"></a>
+            <div class="sell">
+                <a href="">Đường Đen Sữa Đá</a>
+                <p>49.000 đ</p>
+            </div>
+        </div>
+        <div class="col">
+            <a href=""><img src="img/đường đen sd.webp" alt="" width="250px"></a>
+            <div class="sell">
+                <a href="">Đường Đen Sữa Đá</a>
+                <p>49.000 đ</p>
+            </div>
+        </div>
+        <div class="col">
+            <a href=""><img src="img/đường đen sd.webp" alt="" width="250px"></a>
+            <div class="sell">
+                <a href="">Đường Đen Sữa Đá</a>
+                <p>49.000 đ</p>
+            </div>
+        </div>
+        <div class="col">
+            <a href=""><img src="img/đường đen sd.webp" alt="" width="250px"></a>
+            <div class="sell">
+                <a href="">Đường Đen Sữa Đá</a>
+                <p>49.000 đ</p>
+            </div>
+        </div>
+    </div>
+    <div class="cf">
+        <div class="col">
+            <a href=""><img src="img/đường đen sd.webp" alt="" width="250px"></a>
+            <div class="sell">
+                <a href="">Đường Đen Sữa Đá</a>
+                <p>49.000 đ</p>
+            </div>
+        </div>
+        <div class="col">
+            <a href=""><img src="img/đường đen sd.webp" alt="" width="250px"></a>
+            <div class="sell">
+                <a href="">Đường Đen Sữa Đá</a>
+                <p>49.000 đ</p>
+            </div>
+        </div>
+        <div class="col">
+            <a href=""><img src="img/đường đen sd.webp" alt="" width="250px"></a>
+            <div class="sell">
+                <a href="">Đường Đen Sữa Đá</a>
+                <p>49.000 đ</p>
+            </div>
+        </div>
+        <div class="col">
+            <a href=""><img src="img/đường đen sd.webp" alt="" width="250px"></a>
+            <div class="sell">
+                <a href="">Đường Đen Sữa Đá</a>
+                <p>49.000 đ</p>
+            </div>
+        </div>
+    </div>
+</div>
