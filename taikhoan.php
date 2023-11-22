632be8f5f19ecd6bda5479ce7e801f172784aa24
Chanhk4 <150499432+Chanhk4@users.noreply.github.com>
2023-11-22 13:35:35 +0700

    taikhoan.php

--- taikhoan.php
+++ taikhoan.php
@@ -3,14 +3,23 @@ function dang_ky($email, $user, $pass, $sdt) {
     $sql = "insert into taikhoan (taikhoan, matkhau, sdt,email ) values ('$user', '$pass', '$sdt','$email')";
     pdo_execute($sql);
 }
+
 function dang_nhap($user, $pass) {
     $sql = "select * from taikhoan where taikhoan='$user' and matkhau='$pass'";
     $taikhoan = pdo_query_one($sql);
     return $taikhoan;
 }
+
 function quen_mat_khau($email) {
     $sql = "select * from taikhoan where email='".$email."' "  ;
     $taikhoan = pdo_query_one($sql);
     return $taikhoan;
 }
+
+function edit_taikhoan($id,$hovaten,$user,$pass,$phone,$email){
+    $sql = "UPDATE taikhoan SET  hovaten = '".$hovaten."', taikhoan = '".$user."', matkhau = '".$pass."',sdt = '".$phone."',email = '".$email."' WHERE id=".$id;    
+    pdo_execute($sql);
+    
+}
+
 ?>
